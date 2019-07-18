8da5b431136a4992550def9a2d28e4843ea90f03
andr-off <and_ron@mail.ru>
2019-07-18 18:45:43 +0500

    add genDiff function

--- src/bin/gendiff.js
+++ src/bin/gendiff.js
@@ -1,5 +1,23 @@
 #!/usr/bin/env node
 
-import main from '..';
+import commander from 'commander';
+import genDiff from '..';
+
+const program = new commander.Command();
+
+const main = () => {
+  program
+    .version('0.0.1')
+    .description('Compares two configuration files and shows a difference.')
+    .option('-f, --format [type]', 'output format')
+    .arguments('<firstConfig> <secondConfig>')
+    .action((firstConfig, secondConfig) => {
+      const diff = genDiff(firstConfig, secondConfig);
+
+      console.log(diff);
+    });
+
+  program.parse(process.argv);
+};
 
 main();
--- src/index.js
+++ src/index.js
@@ -1,13 +1,47 @@
-import commander from 'commander';
+import fs from 'fs';
+import _ from 'lodash';
 
-const program = new commander.Command();
+const compare = (object1, object2) => {
+  const keys = [...Object.keys(object1), ...Object.keys(object2)];
+  const uniqKeys = Array.from(new Set(keys)).sort();
 
-export default () => {
-  program
-    .version('0.0.1')
-    .description('Compares two configuration files and shows a difference.')
-    .option('-f, --format [type]', 'output format')
-    .arguments('<firstConfig> <secondConfig>');
+  const result = uniqKeys.map((key) => {
+    if (_.has(object2, key) && object1[key] === object2[key]) {
+      return `    ${key}: ${object1[key]}`;
+    }
 
-  program.parse(process.argv);
+    if (!_.has(object1, key)) {
+      return `  + ${key}: ${object2[key]}`;
+    }
+
+    if (!_.has(object2, key)) {
+      return `  - ${key}: ${object1[key]}`;
+    }
+
+    return `  + ${key}: ${object2[key]}\n  - ${key}: ${object1[key]}`;
+  }).join('\n');
+
+  return result;
 };
+
+const genDiff = (pathToConfig1, pathToConfig2) => {
+  if (!fs.existsSync(pathToConfig1)) {
+    console.log(`No such file: ${pathToConfig1}`);
+    return '';
+  }
+
+  if (!fs.existsSync(pathToConfig2)) {
+    console.log(`No such file: ${pathToConfig2}`);
+    return '';
+  }
+
+  const config1 = fs.readFileSync(pathToConfig1);
+  const config2 = fs.readFileSync(pathToConfig2);
+
+  const obj1 = JSON.parse(config1);
+  const obj2 = JSON.parse(config2);
+
+  return `{\n${compare(obj1, obj2)}\n}`;
+};
+
+export default genDiff;
