2aa6f4109ea11ae46c3a7bf35b5df6537a6639b2
andr-off <and_ron@mail.ru>
2019-07-19 06:16:12 +0500

    add tests for ini parser

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -4,28 +4,34 @@ import genDiff from '../src';
 const pathToExpected = `${__dirname}/__fixtures__/expected.txt`;
 const expected = fs.readFileSync(pathToExpected, 'utf-8');
 
-test('simple jsons', () => {
-  const pathToConfig1 = `${__dirname}/__fixtures__/jsonConfigBefore.json`;
-  const pathToConfig2 = `${__dirname}/__fixtures__/jsonConfigAfter.json`;
-  const pathToConfig3 = `${__dirname}/__fixtures__/jsonConfigBefore2.json`;
-  const pathToConfig4 = `${__dirname}/__fixtures__/jsonConfigAfter2.json`;
+const pathToJsonConfig1 = `${__dirname}/__fixtures__/jsonConfigBefore.json`;
+const pathToJsonConfig2 = `${__dirname}/__fixtures__/jsonConfigAfter.json`;
+const pathToJsonConfig3 = `${__dirname}/__fixtures__/jsonConfigBefore2.json`;
+const pathToJsonConfig4 = `${__dirname}/__fixtures__/jsonConfigAfter2.json`;
 
-  const diff = genDiff(pathToConfig1, pathToConfig2);
-  const diff2 = genDiff(pathToConfig3, pathToConfig4);
+const pathToYamlConfig1 = `${__dirname}/__fixtures__/yamlConfigBefore.yml`;
+const pathToYamlConfig2 = `${__dirname}/__fixtures__/yamlConfigAfter.yml`;
+const pathToYamlConfig3 = `${__dirname}/__fixtures__/yamlConfigBefore2.yml`;
+const pathToYamlConfig4 = `${__dirname}/__fixtures__/yamlConfigAfter2.yml`;
 
-  expect(diff).toBe(expected);
-  expect(diff2).toBe(expected);
-});
+const pathToIniConfig1 = `${__dirname}/__fixtures__/iniConfigBefore.ini`;
+const pathToIniConfig2 = `${__dirname}/__fixtures__/iniConfigAfter.ini`;
+const pathToIniConfig3 = `${__dirname}/__fixtures__/iniConfigBefore2.ini`;
+const pathToIniConfig4 = `${__dirname}/__fixtures__/iniConfigAfter2.ini`;
 
-test('simple yamls', () => {
-  const pathToConfig1 = `${__dirname}/__fixtures__/yamlConfigBefore.yml`;
-  const pathToConfig2 = `${__dirname}/__fixtures__/yamlConfigAfter.yml`;
-  const pathToConfig3 = `${__dirname}/__fixtures__/yamlConfigBefore2.yml`;
-  const pathToConfig4 = `${__dirname}/__fixtures__/yamlConfigAfter2.yml`;
+const table = [
+  ['jsons', pathToJsonConfig1, pathToJsonConfig2, pathToJsonConfig3, pathToJsonConfig4],
+  ['yamls', pathToYamlConfig1, pathToYamlConfig2, pathToYamlConfig3, pathToYamlConfig4],
+  ['inis', pathToIniConfig1, pathToIniConfig2, pathToIniConfig3, pathToIniConfig4],
+];
 
-  const diff = genDiff(pathToConfig1, pathToConfig2);
-  const diff2 = genDiff(pathToConfig3, pathToConfig4);
+test.each(table)(
+  'simple %s',
+  (testName, path1, path2, path3, path4) => {
+    const diff = genDiff(path1, path2);
+    const diff2 = genDiff(path3, path4);
 
-  expect(diff).toBe(expected);
-  expect(diff2).toBe(expected);
-});
+    expect(diff).toBe(expected);
+    expect(diff2).toBe(expected);
+  },
+);
