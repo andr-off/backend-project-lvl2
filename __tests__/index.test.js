b80816450e1168f0faf8a87b1419229b7863558c
andr-off <and_ron@mail.ru>
2019-07-20 20:38:46 +0500

    fix tests

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,37 +1,66 @@
 import fs from 'fs';
+import path from 'path';
 import genDiff from '../src';
 
-const pathToExpected = `${__dirname}/__fixtures__/expected.txt`;
-const expected = fs.readFileSync(pathToExpected, 'utf-8');
+const files = fs.readdirSync(`${__dirname}/__fixtures__/`);
+const pathes = files.map(file => `${__dirname}/__fixtures__/${file}`);
 
-const pathToJsonConfig1 = `${__dirname}/__fixtures__/jsonConfigBefore.json`;
-const pathToJsonConfig2 = `${__dirname}/__fixtures__/jsonConfigAfter.json`;
-const pathToJsonConfig3 = `${__dirname}/__fixtures__/jsonConfigBefore2.json`;
-const pathToJsonConfig4 = `${__dirname}/__fixtures__/jsonConfigAfter2.json`;
+const jsons = pathes.filter(p => path.extname(p) === '.json');
+const simpleJsons = jsons.filter(p => !p.includes('complex'));
+const complexJsons = jsons.filter(p => p.includes('complex'));
 
-const pathToYamlConfig1 = `${__dirname}/__fixtures__/yamlConfigBefore.yml`;
-const pathToYamlConfig2 = `${__dirname}/__fixtures__/yamlConfigAfter.yml`;
-const pathToYamlConfig3 = `${__dirname}/__fixtures__/yamlConfigBefore2.yml`;
-const pathToYamlConfig4 = `${__dirname}/__fixtures__/yamlConfigAfter2.yml`;
+const yamls = pathes.filter(p => path.extname(p) === '.ini');
+const simpleYamls = yamls.filter(p => !p.includes('complex'));
+const complexYamls = yamls.filter(p => p.includes('complex'));
 
-const pathToIniConfig1 = `${__dirname}/__fixtures__/iniConfigBefore.ini`;
-const pathToIniConfig2 = `${__dirname}/__fixtures__/iniConfigAfter.ini`;
-const pathToIniConfig3 = `${__dirname}/__fixtures__/iniConfigBefore2.ini`;
-const pathToIniConfig4 = `${__dirname}/__fixtures__/iniConfigAfter2.ini`;
+const inis = pathes.filter(p => path.extname(p) === '.ini');
+const simpleInis = inis.filter(p => !p.includes('complex'));
+const complexInis = inis.filter(p => p.includes('complex'));
 
-const table = [
-  ['jsons', pathToJsonConfig1, pathToJsonConfig2, pathToJsonConfig3, pathToJsonConfig4],
-  ['yamls', pathToYamlConfig1, pathToYamlConfig2, pathToYamlConfig3, pathToYamlConfig4],
-  ['inis', pathToIniConfig1, pathToIniConfig2, pathToIniConfig3, pathToIniConfig4],
-];
+const [
+  expected1,
+  expected2,
+  expected3,
+] = pathes.filter(p => path.extname(p) === '.txt')
+  .map(p => fs.readFileSync(p, 'utf-8'));
 
-test.each(table)(
+const simpleConfigs = {
+  jsons: simpleJsons,
+  yamls: simpleYamls,
+  inis: simpleInis,
+};
+
+const complexConfigs = {
+  jsons: complexJsons,
+  yamls: complexYamls,
+  inis: complexInis,
+};
+
+const makeTable = configs => Object
+  .entries(configs)
+  .map(([key, value]) => [key, ...value]);
+
+const table1 = makeTable(simpleConfigs);
+const table2 = makeTable(complexConfigs);
+
+test.each(table1)(
   'simple %s',
   (testName, path1, path2, path3, path4) => {
-    const diff = genDiff(path1, path2);
-    const diff2 = genDiff(path3, path4);
+    const diff = genDiff(path3, path1);
+    const diff2 = genDiff(path4, path2);
 
-    expect(diff).toBe(expected);
-    expect(diff2).toBe(expected);
+    expect(diff).toBe(expected1);
+    expect(diff2).toBe(expected1);
+  },
+);
+
+test.each(table2)(
+  'complex %s',
+  (testName, path1, path2, path3, path4) => {
+    const diff = genDiff(path3, path1);
+    const diff2 = genDiff(path4, path2);
+
+    expect(diff).toBe(expected2);
+    expect(diff2).toBe(expected3);
   },
 );
