8f3d9dbe16b45b5e7a1b1c38f73e6b3cee15e1e1
andr-off <and_ron@mail.ru>
2019-07-21 10:35:39 +0500

    simplify tests

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,66 +1,20 @@
 import fs from 'fs';
-import path from 'path';
 import genDiff from '../src';
 
-const files = fs.readdirSync(`${__dirname}/__fixtures__/`);
-const pathes = files.map(file => `${__dirname}/__fixtures__/${file}`);
+const expected = fs.readFileSync(`${__dirname}/__fixtures__/expected.txt`, 'utf-8');
 
-const jsons = pathes.filter(p => path.extname(p) === '.json');
-const simpleJsons = jsons.filter(p => !p.includes('complex'));
-const complexJsons = jsons.filter(p => p.includes('complex'));
+const table = ['json', 'yml', 'ini'].map((ext) => {
+  const path1 = `${__dirname}/__fixtures__/before.${ext}`;
+  const path2 = `${__dirname}/__fixtures__/after.${ext}`;
 
-const yamls = pathes.filter(p => path.extname(p) === '.ini');
-const simpleYamls = yamls.filter(p => !p.includes('complex'));
-const complexYamls = yamls.filter(p => p.includes('complex'));
+  return [ext, path1, path2];
+});
 
-const inis = pathes.filter(p => path.extname(p) === '.ini');
-const simpleInis = inis.filter(p => !p.includes('complex'));
-const complexInis = inis.filter(p => p.includes('complex'));
+test.each(table)(
+  '%s configs',
+  (testName, path1, path2) => {
+    const diff = genDiff(path1, path2);
 
-const [
-  expected1,
-  expected2,
-  expected3,
-] = pathes.filter(p => path.extname(p) === '.txt')
-  .map(p => fs.readFileSync(p, 'utf-8'));
-
-const simpleConfigs = {
-  jsons: simpleJsons,
-  yamls: simpleYamls,
-  inis: simpleInis,
-};
-
-const complexConfigs = {
-  jsons: complexJsons,
-  yamls: complexYamls,
-  inis: complexInis,
-};
-
-const makeTable = configs => Object
-  .entries(configs)
-  .map(([key, value]) => [key, ...value]);
-
-const table1 = makeTable(simpleConfigs);
-const table2 = makeTable(complexConfigs);
-
-test.each(table1)(
-  'simple %s',
-  (testName, path1, path2, path3, path4) => {
-    const diff = genDiff(path3, path1);
-    const diff2 = genDiff(path4, path2);
-
-    expect(diff).toBe(expected1);
-    expect(diff2).toBe(expected1);
-  },
-);
-
-test.each(table2)(
-  'complex %s',
-  (testName, path1, path2, path3, path4) => {
-    const diff = genDiff(path3, path1);
-    const diff2 = genDiff(path4, path2);
-
-    expect(diff).toBe(expected2);
-    expect(diff2).toBe(expected3);
+    expect(diff).toBe(expected);
   },
 );
