b8c2ae53fb059bda09bfe3977394df2c869b048e
andr-off <and_ron@mail.ru>
2019-07-27 15:10:22 +0500

    add tests

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -3,6 +3,7 @@ import genDiff from '../src';
 
 const expected = fs.readFileSync(`${__dirname}/__fixtures__/expected.txt`, 'utf-8');
 const expected2 = fs.readFileSync(`${__dirname}/__fixtures__/expected-plain.txt`, 'utf-8');
+const expected3 = fs.readFileSync(`${__dirname}/__fixtures__/expected.json`, 'utf-8');
 
 const table = ['json', 'yml', 'ini'].map((ext) => {
   const path1 = `${__dirname}/__fixtures__/before.${ext}`;
@@ -16,8 +17,10 @@ test.each(table)(
   (testName, path1, path2) => {
     const diff = genDiff(path1, path2);
     const diff2 = genDiff(path1, path2, 'plain');
+    const diff3 = genDiff(path1, path2, 'json');
 
     expect(diff).toBe(expected);
     expect(diff2).toBe(expected2);
+    expect(diff3).toBe(expected3);
   },
 );
